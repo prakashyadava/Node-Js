39aad0aef438f34f6e5488510d35504a60adf714
prakashyadava <prakash.kumar.yadava@gmail.com>
2022-03-16 22:33:49 +0530

    passport jwt

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -5,13 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
 var authenticate = require('./authenticate');
-
+var config = require('./config');
 // establishing the connection
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leader');
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
@@ -70,19 +70,20 @@ app.use('/users', usersRouter);
 
 app.use(passport.initialize());
 app.use(passport.session());
-function auth (req, res, next) {
-  console.log(req.user);
 
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
-app.use(auth);
+// function auth (req, res, next) {
+//   console.log(req.user);
+
+//   if (!req.user) {
+//     var err = new Error('You are not authenticated!');
+//     err.status = 403;
+//     next(err);
+//   }
+//   else {
+//         next();
+//   }
+// }
+// app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
